0da6a3e7cbbd349c86db0f3cdf84755a7405d85d
04cicy <cibellycristiny880@gmail.com>
2024-10-29 08:48:33 -0300

    ;;;;;

---
+++ perguntas.js
@@ -0,0 +1,56 @@
+CriaCartao(
+   "Biologia",
+   "O que a biologia estuda?",
+   "E a ciência que estuda a vida e os organismos vivos, sua estrutura, crescimento,funcionamento, reprodução, origem, evolução, distribuição, bem como suas relações com o ambiente e entre si"
+)
+CriaCartao(
+   "Biotecnologia",
+   "O que a biotecnologia estuda?",
+   "Estuda qualquer aplicação tecnológica que utilize sistemasbiológicos, organismos vivos, ou seus derivados, para fabricar ou modificar produtos ou processos para utilização específica"
+)
+CriaCartao(
+    "Geografia",
+    "O que a geografia estuda?",
+    ""
+ )
+ CriaCartao(
+    "Historia",
+    "O que a historia estuda?",
+    ""
+ )
+ CriaCartao(
+    "Quimica",
+    "O que a quimica estua?",
+    ""
+ )
+ CriaCartao(
+    "Fisica",
+    "O que a fisica estuda?",
+    ""
+ )
+ CriaCartao(
+    "Ingles",
+    "O que e o verbo to be?",
+    ""
+ )
+ CriaCartao(
+    "Portugues",
+    "O que a lingua portuguesa estuda?",
+    ""
+ )
+ CriaCartao(
+    "Sociologia",
+    "o que a sociologia estuda?",
+    ""
+ )
+ varCriaCartao(
+    "Maremarica",
+    "O que a matematica estuda?",
+    ""
+ )
+ CriaCartao(
+    "Robotica",
+    "O que a robotica estuda?",
+    ""
+ )
+ 
