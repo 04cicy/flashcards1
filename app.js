39eae60f9f72f3933fcf6427cf022af2d74b4329
04cicy <cibellycristiny880@gmail.com>
2024-11-05 08:43:18 -0300

    yh

---
+++ app.js
@@ -0,0 +1,18 @@
+function criaCartao(caregaria, perginta, resposta)
+    let cartao = document.createElement("article")
+    cartao.className = "cartao"
+    cartao.innerHTML = `
+     <div class="cartao_conteudo"
+                   <h3>${categoria}</h3>
+                   <div class="cartao_conteudo_pergonta">
+                       <p>${pergunta}</p>
+                   </div>
+                   <div class="cartao_conteudo_resposta">
+                       <p>${pergunta}</p>
+                   </dic>
+
+               </div>
+
+   let conteiner = document. getElemrntById("container")
+   container.appendChild(carta)
+}
